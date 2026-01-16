a25424dd6cb08e76795adfa9544a7547bde2e9a6
Nanashi1215 <mcj9363@gmail.com>
2026-01-16 14:05:39 +0800

    leaderboard-v1

--- Assets/Scripts/LearnModeController.cs
+++ Assets/Scripts/LearnModeController.cs
@@ -6,13 +6,15 @@ public class LearnModeController : MonoBehaviour
 {
     [Header("UI References")]
     public Button[] selectionButtons;   // 17 buttons
-    public Image displayImage;          // Image to show in Learn Mode
+    public Image displayImage;          // Main image to show in Learn Mode
+    public Image iconImage;             // Icon image for each topic
     public Button nextButton;           // Next button
     public Button prevButton;           // Previous button
-    public TMP_Text titleText;              // Title text at the top
+    public TMP_Text titleText;          // Title text at the top
 
     [Header("Content")]
-    public Sprite[] learnSprites;       // Array of 17 sprites
+    public Sprite[] learnSprites;       // Array of 17 main sprites
+    public Sprite[] topicIcons;         // Array of 17 icon sprites
     public string[] topicTitles;        // Array of 17 topic titles
 
     private int currentIndex = 0;
@@ -30,8 +32,9 @@ public class LearnModeController : MonoBehaviour
         nextButton.onClick.AddListener(OnNext);
         prevButton.onClick.AddListener(OnPrev);
 
-        // Hide image and title at start
+        // Hide image, icon, and title at start
         displayImage.gameObject.SetActive(false);
+        iconImage.gameObject.SetActive(false);
         titleText.gameObject.SetActive(false);
     }
 
@@ -60,9 +63,20 @@ public class LearnModeController : MonoBehaviour
     void ShowContent()
     {
         displayImage.gameObject.SetActive(true);
+        iconImage.gameObject.SetActive(true);
         titleText.gameObject.SetActive(true);
 
+        // Show main image
         displayImage.sprite = learnSprites[currentIndex];
+
+        // Show icon
+        if (topicIcons != null && currentIndex < topicIcons.Length)
+        {
+            iconImage.sprite = topicIcons[currentIndex];
+        }
+
+        // Show title
         titleText.text = topicTitles[currentIndex];
     }
 }
+    
